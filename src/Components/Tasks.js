f530f9bcecbd4b397ecb3ff494e1031105c27a2c
YaroslavW <doctor355@yahoo.com>
2019-03-25 14:03:36 +0300

    Create Navbar and Tasks Component

---
+++ src/Components/Tasks.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// Access all components from `muicss/react` module
+import { Panel } from 'muicss/react';
+
+
+
+class Tasks extends Component {
+    render() {
+        return (
+            <Panel>
+                TASKS
+            </Panel>
+        );
+    }
+}
+
+export default Tasks;
+
